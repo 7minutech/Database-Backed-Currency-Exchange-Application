812a80c6898255e94d118fbf064a1453680d9934
7minutech <elijohnson1107@gmail.com>
2025-10-24 19:29:42 -0500

    feat: insertRate() helper to insert rate into db

--- utils/conversion_helper.js
+++ utils/conversion_helper.js
@@ -1,4 +1,7 @@
 import { db } from "../store/db.js"
+import { fetchExchangeRate, addParamsToOptions } from '../api/convertApi.js';
+import { rates_db } from "../store/lab3_rates_20250911.js"
+
 
 export function conversionResp(from, to, amount, rate, date, success){
     const today = new Date();
@@ -23,9 +26,37 @@ export function fetchRate(resp, params){
             console.error(err.message);
         }
         if (!row){
-            return resp.status(500).json(conversionResp(to, from, amount, undefined, date, false));
+            fetchExchangeRate(addParamsToOptions(params), insertRate, resp, params)
+            return
+            // resp.status(500).json(conversionResp(to, from, amount, undefined, date, false));
         }
         let result = row.rate * amount
         resp.json(conversionResp(to, from, amount, result, date, true))
+    });
+}
+
+export function insertRate(err, data, resp, params){
+    const { to, from, amount, date } = params; 
+
+    if (err){
+        console.error("error: fetching conversion from api")
+        return
+    }
+    if (data.message) {
+        console.error(data)
+        resp.status(500).json(conversionResp(to, from, amount, undefined, date, false))
+        return
+    }
+    let symbol = to
+    let rate = data.rates.to
+    let queryDate = data.date
+    db.run(`INSERT INTO rate(currency_id, date, rate) VALUES(?, ?, ?)`, [symbol, rate, queryDate], (err) => {
+        if (err) {
+            console.error(err.message);
+        } 
+        else {
+            console.log("Rows updated:");
+            fetchRate(resp, params)
+        }
     });
 }
